d041cac0cb416aa6969f1a71544b993dd580745c
Léo Ludwig <leoludwig26@gmail.com>
2025-04-24 21:12:44 -0300

    feat: adicionar implementação básica da UsersPage

--- lib/screens/users_page.dart
+++ lib/screens/users_page.dart
@@ -1,1 +1,12 @@
- 
+import 'package:flutter/material.dart';
+
+class UsersPage extends StatelessWidget {
+  const UsersPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Página de Usuários'),
+    );
+  }
+}
